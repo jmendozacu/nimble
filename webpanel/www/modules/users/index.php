2c59c60812f3917def04a82b048086fe6cdd4af1
Igknighted <sam.igknighted@gmail.com>
2014-06-01 11:08:04 +0000

    Trying to ignore session files

--- webpanel/www/modules/users/index.php
+++ webpanel/www/modules/users/index.php
@@ -5,6 +5,7 @@ $user->loginRequired();
 
 function iterate_owned_users($system_username){
 	global $db;
+	global $xtpl;
 
 	$owned_users = $db->openTable('users');
 	$owned_users->getDataByOwnedBy($system_username);
@@ -18,4 +19,21 @@ function iterate_owned_users($system_username){
 	
 }
 
-iterate_owned_users($user->getSystemUsername());
+
+
+if($user->getPrivRootLevel() == 'Y'){
+
+	$owned_users = $db->openTable('users');
+	$owned_users->getData();
+
+	do {
+
+		echo $owned_users->getSystemUsername();
+
+	}while($owned_users->next());
+
+}else{
+
+	iterate_owned_users($user->getSystemUsername());
+
+}
